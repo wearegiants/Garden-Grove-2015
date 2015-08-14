871692471366596eb4fea2d9f78bf245ab7b499a
Craig Fowler <fowler@craigsnwhotness.attlocal.net>
2015-08-14 06:06:02 -0700

    updated the about page map display

--- page.php
+++ page.php
@@ -2,16 +2,16 @@
 <?php get_template_part('templates/page', 'header'); ?>
 <div class="frame" id="main-wrap">
 
+<div id="map-deal" class="bit-1">
+<?php the_post_thumbnail( 'original', array( 'class' => 'img-responsive' ) ); ?>
 <?php if( have_rows('map_notes') ):
-echo '<div id="map-deal" class="bit-1">';
-the_post_thumbnail( 'original', array( 'class' => 'img-responsive' ) );
 while ( have_rows('map_notes') ) : the_row(); ?>
 
 <div class="bit-5 item"><?php the_sub_field('map_notes_node'); ?></div>
 
 <?php endwhile;
-echo '</div>';
 else : endif; ?>
+</div>
 
 <div class="bit-66" id="main">
 <?php get_template_part('templates/page', 'content'); ?>
